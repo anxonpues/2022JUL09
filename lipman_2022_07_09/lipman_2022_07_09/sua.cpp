dfebb75170f16c5eeb0caecc65aa71de21252d9d
Anxon Pues <43876773+anxonpues@users.noreply.github.com>
2022-07-11 20:31:40 +0200

    const top-level and low_level ptr and ref

--- lipman_2022_07_09/lipman_2022_07_09/sua.cpp
+++ lipman_2022_07_09/lipman_2022_07_09/sua.cpp
@@ -24,6 +24,7 @@ int main()
 	Dealing with types
 	type aliases
 	*/
+
 	typedef double wages; // wages is a synonym for double
 	typedef wages base, * p; // base is a synonym for double, p for double*
 	using SI = objetoAnonimo; // SI is a synonym for objetoAnonimo
@@ -41,6 +42,16 @@ int main()
 	auto i = 0, * pin = &i; // ok: i is int and p is a pointer to int
 	auto sz = 0.0, pi = 3.14; // error: inconsistent types for sz and pi 
 							  //( lo he cambiado a sz = 0.0  antes era sz = 0 (int))
+
+
+	int ii = 0, & r = ii;
+	auto a = r; // a is an int (r is an alias for i, which has type int)
+
+	const int ci = ii, & cr = ci;
+	auto b = ci; // b is an int (top-level const in ci is dropped)
+	auto c = cr; // c is an int (cr is an alias for ci whose const is top-level)
+	auto d = &i; // d is an int*(& of an int object is int*)
+	auto e = &ci; // e is const int*(& of a const object is low-level const)
 
 
 
